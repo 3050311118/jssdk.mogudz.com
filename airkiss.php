024aae2d82f062eb5d40655959fd3158e5925f22
3050311118 <844725532@qq.com>
2017-03-08 15:52:47 +0800

    Update airkiss.php

--- airkiss.php
+++ airkiss.php
@@ -41,6 +41,7 @@ $signPackage = $jssdk->GetSignPackage();
   };
 wx.config(config);
 wx.ready(function() {
+  alert(config.timestamp+" "+config.appId+" "+config.nonceStr+" "+config.signature);
   $(".connectWifi").click(function(){
     wx.invoke('configWXDeviceWiFi', {}, function(res) {
       if (res.err_msg == 'configWXDeviceWiFi:ok') {

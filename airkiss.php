fbae632702ac83661c7ef3724ab20dfc0ee5b3bd
3050311118 <844725532@qq.com>
2017-03-08 15:47:40 +0800

    Update airkiss.php

--- airkiss.php
+++ airkiss.php
@@ -32,7 +32,7 @@ $signPackage = $jssdk->GetSignPackage();
     beta: true,
     // debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
-    timestamp: <?php echo $signPackage["timestamp"];?>,
+    timestamp: '<?php echo $signPackage["timestamp"];?>',
     nonceStr: '<?php echo $signPackage["nonceStr"];?>',
     signature: '<?php echo $signPackage["signature"];?>',
   jsApiList: [
